45b56f362ea5bb2455733db7248e2fd413450c7f
emily yim <emilyyim@MacBook-Pro.local>
2023-08-29 15:55:50 -0700

    create hat model

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Team:
 
-* Person 1 - Which microservice?
-* Person 2 - Which microservice?
+* Person 1 - Which microservice? -- Emily - Hats
+* Person 2 - Which microservice? -- Brian - Shoes
 
 ## Design
 
--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    closet_name = models.CharField(max_length=200)
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    picUrl = models.URLField(null=True)
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
