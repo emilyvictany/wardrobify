271a0a83cfc820615665e5bba4c0f26a14924044
Brian <bmla.pt@gmail.com>
2023-08-30 19:09:38 -0700

    delete button works

--- ghi/app/src/ShoeList.jsx
+++ ghi/app/src/ShoeList.jsx
@@ -2,9 +2,13 @@ import { NavLink } from 'react-router-dom';
 import React, {useEffect, useState} from "react";
 import './index.css'
 
+
+
 const ShoeList = (props) => {
+    // This code is to get and show the shoe data in a list
     // console.log(props)
     const [shoes, setShoes] = useState([]);
+    const [shoe, setShoe] = useState('');
     const fetchData = async () => {
         const allShoesUrl = `http://localhost:8080/api/bins/shoes/`;
 
@@ -18,7 +22,7 @@ const ShoeList = (props) => {
                 setShoes(shoeList)
             }
         } catch (e) {
-            // console.error(e)
+            console.error(e)
         }
 
 
@@ -27,7 +31,27 @@ const ShoeList = (props) => {
     useEffect(() => {
         fetchData();
       }, []);
-    console.log(shoes)
+    // console.log(shoes[1].href)
+
+    const handleDelete = async (href) => {
+        // const shoeUrl = `http://localhost:8080/${shoes}`
+        const newShoeList = shoes.filter((shoe) => shoe.href != href);
+        console.log(newShoeList);
+        const url = `http://localhost:8080${href}`;
+        const deleteOptions = {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: null
+        }
+        const deleteResponse = await fetch(url, deleteOptions);
+        if (deleteResponse.ok) {
+            setShoes(newShoeList);
+        }
+        console.log(newShoeList);
+    }
+
     return (
         <div>
             <NavLink className="nav-link" to="/shoes/new">Add Shoes</NavLink>
@@ -40,6 +64,7 @@ const ShoeList = (props) => {
                         <th>Color</th>
                         <th>Bin Number</th>
                         <th>Closet Name</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -52,6 +77,9 @@ const ShoeList = (props) => {
                     <td>{ shoe.color }</td>
                     <td>{ shoe.bin.number }</td>
                     <td>{ shoe.bin.closet_name } </td>
+                    <td>
+                        <button className='delete' type='button' onClick={() => handleDelete(shoe.href)}> Delete </button>
+                    </td>
                   </tr>
                   );
                 })}
